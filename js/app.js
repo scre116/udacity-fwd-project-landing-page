9fe06f1a43058a3654e098231ff33a577e8ad2cc
Eugen Schreiner <eugen.schreiner@bertelsmann.de>
2023-02-10 21:54:55 +0100

    feat: activate viewed section

--- js/app.js
+++ js/app.js
@@ -20,15 +20,16 @@
 
 /**
  * Define Global Variables
- * 
-*/
+ */
 
+// navigable sections
+const sections = document.querySelectorAll('[data-nav]');
 
 /**
  * End Global Variables
  * Start Helper Functions
- * 
-*/
+ *
+ */
 
 
 
@@ -41,8 +42,6 @@
 // build the nav
 function loadNavigation() {
     const documentFragment = document.createDocumentFragment();
-    const sections = document.querySelectorAll('[data-nav]');
-
     for (const section of sections) {
         const listItem = document.createElement('li');
         listItem.textContent = section.dataset.nav;
@@ -52,12 +51,52 @@ function loadNavigation() {
         documentFragment.appendChild(listItem);
     }
 
-    let navbar = document.getElementById('navbar__list');
+    const navbar = document.getElementById('navbar__list');
     navbar.appendChild(documentFragment);
 }
 
 // Add class 'active' to section when near top of viewport
+function activateViewedSection() {
 
+
+    function sectionIsVisible(section) {
+        const top = section.getBoundingClientRect().top;
+        const bottom = section.getBoundingClientRect().bottom;
+        return top < window.visualViewport.height && bottom > 0;
+    }
+
+    function getFirstVisibleSection() {
+
+        // find the section which is displayed at the relative top of the screen
+        for (const section of sections) {
+            // this is our relative top of the screen. It is at 25% from the real top.
+            const viewPortTop = window.visualViewport.height / 4;
+            const top = section.getBoundingClientRect().top;
+            const bottom = section.getBoundingClientRect().bottom;
+
+            if (top < viewPortTop && bottom > viewPortTop) {
+                return section;
+            }
+        }
+
+        // no section is (sufficiently) visible. Return the first section, if it is visible at all
+        if (sectionIsVisible(sections.item(0))) {
+            return sections.item(0);
+        }
+
+        return null;
+    }
+
+    const firstVisibleSection = getFirstVisibleSection();
+
+    for (const section of sections) {
+        if (section === firstVisibleSection) {
+            section.classList.add('active');
+        } else {
+            section.classList.remove('active');
+        }
+    }
+}
 
 // Scroll to anchor ID using scrollTO event
 
@@ -71,8 +110,13 @@ function loadNavigation() {
 // Build menu 
 document.addEventListener('DOMContentLoaded', loadNavigation);
 
+// Set sections as active
+document.addEventListener('scroll', activateViewedSection);
+document.addEventListener('resize', activateViewedSection);
+document.addEventListener('DOMContentLoaded', activateViewedSection);
+
+
 // Scroll to section on link click
 
-// Set sections as active
 
 
